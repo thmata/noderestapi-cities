10423ac70203ff3467323bde7b56daa1aa063f4e
Thiago Mata <taiagomata@gmail.com>
2023-05-17 20:59:24 -0300

    feat: trantando erros e adicionando traducao

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import "./shared/services/TraducoesYup";
 
 // Router é como se fosse o plugin do fastFy, server para adicionar as rotas na aplicação.
 import { router } from "./routes";
--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,21 +1,33 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 
 const bodyValidation = yup.object().shape({
   name: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request, res: Response) => {
   let validatedData = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
   } catch (error) {
     const yupError = error as yup.ValidationError;
+    const validationErros: Record<string, string> = {};
 
-    return res.json({
+    // VALIDAÇÃO DE ERROS COMPLETOS.
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+
+      validationErros[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
       errors: {
-        default: yupError.message,
+        default: validationErros,
       },
     });
   }
