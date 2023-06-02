dd119076811bbbc245a71c312c8380c42079298b
Thiago Mata <taiagomata@gmail.com>
2023-06-01 21:34:34 -0300

    feat: explanation about tests

--- tests/cidades/Create.test.ts
+++ tests/cidades/Create.test.ts
@@ -5,9 +5,21 @@ import { testServer } from "../jest.setup";
 describe("Cidades - Create", () => {
   it("Cria registro", async () => {
     const res1 = await testServer.post("/cidades").send({
-      nome: "Pernambuco",
+      name: "Pernambuco",
     });
 
-    expect(typeof res1.body).toEqual("number");
+    expect(res1.body).toEqual("Não Implementado");
+  });
+
+  // IT É O NOME DO TESTE
+  it("Criando registro com nome curto", async () => {
+    // FAZENDO UM POST E ENVIANDO A RESPOSTA DESSA REQUISIÇÃO PARA O RES1 PARA FAZER A COMPARAÇÃO EMBAIXO
+    const res1 = await testServer.post("/cidades").send({
+      name: "Pe",
+    });
+
+    // EXPECT É O QUE É ESPERADO E DEPOIS É O QUE QUEREMOS QUE RETORNE.
+    // expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(res1.body).toHaveProperty("errors.body.name");
   });
 });
