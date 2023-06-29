0f10ef0f58e0c25ce43e61b5071108efe4de32e9
Thiago Mata <taiagomata@gmail.com>
2023-06-28 21:27:59 -0300

    fix: ajuste knex para o jest

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,4 +1,15 @@
 import { server } from "../src/server/Server";
+
 import supertest from "supertest";
+import { Knex } from "../src/server/database/knex";
+
+// PARA FUNCIONAR OS TESTES JUNTO COM KNEX
+beforeAll(async () => {
+    await Knex.migrate.latest()
+})
+
+afterAll(async () => {
+    await Knex.destroy
+})
 
 export const testServer = supertest(server);
