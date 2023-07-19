7a31ce411b9e333108a3ce371c9973de1840a34b
Thiago Mata <taiagomata@gmail.com>
2023-07-18 21:26:14 -0300

    feat: criado provider de deletar pelo id

--- src/server/providers/cidades/DeleteById.ts
+++ src/server/providers/cidades/DeleteById.ts
@@ -7,7 +7,7 @@ export const DeleteById = async (id: number) => {
     try{
         console.log(id, "ID do DELETE")
         const result = await Knex(ETableNames.cidade).where('id', '=', id).del()
-        if(result > 0) return 
+        if(result > 0) return true 
 
         return new Error('Erro ao deletar Registro')
     } catch(error) {
