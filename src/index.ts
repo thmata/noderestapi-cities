913e681dbad1acb3a2ddf641a33f79bcd23f6504
Thiago Mata <taiagomata@gmail.com>
2023-08-02 20:04:03 -0300

    fix: ajustes de env e packges

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 import { server } from "./server/Server";
+import { Knex } from "./server/database/knex";
 
-server.listen(3000, () => {
-  console.log(`App Listening on port`);
-});
+const startServer = () => {
+  server.listen(3000, () => {
+    console.log(`App Listening on port`);
+  });
+};
+
+// VALIDAÇÃO PARA SABER SE ESTÁ EM AMBIENTE DE PRODUÇÃO OU DESENVOLVIMENTO
+if (process.env.IS_LOCALHOST !== "true") {
+  // PRIMEIRA COISA QUE SERÁ FEITA QUANDO INICIAR O SERVIDOR É RODAR AS ULTIMAS MIGRATIONS PARA DEPOIS STARTAR O SERVER.
+  Knex.migrate
+    .latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
