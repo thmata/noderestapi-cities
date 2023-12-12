e6e08bbbb1b8a05dc7dfb6e0de0615561f73e8cf
thiago.mata@agenciam3.com <taiagomata@gmail.com>
2023-12-12 00:15:41 -0300

    feat: criado testes pessosas deletebyid e getall

--- tests/pessoas/DeleteById.test.ts
+++ tests/pessoas/DeleteById.test.ts
@@ -1,21 +1,34 @@
 import { StatusCodes } from "http-status-codes";
 import { testServer } from "../jest.setup";
 
-describe("Cidades - Deletar Cidade Pelo ID", () => {
+describe("Pessoas - Deletar Pessoa Pelo ID", () => {
+  let cidadeId: number | undefined = undefined;
+
+  beforeAll(async () => {
+    const resCidade = await testServer.post("/cidades").send({
+      nome: "Teste",
+    });
+
+    cidadeId = resCidade.body;
+  });
+
   it("Apagar Registro", async () => {
-    const res1 = await testServer.post("/cidades").send({
-      name: "Pernambuco",
+    const res1 = await testServer.post("/pessoas").send({
+      name: "Thiago",
+      sobrenome: "Mata",
+      email: "taiagomata@gmail.com",
+      cidadeId,
     });
 
     expect(res1.statusCode).toEqual(StatusCodes.CREATED);
 
-    const resApagada = await testServer.delete(`/cidades/${res1.body}`);
+    const resApagada = await testServer.delete(`/pessoas/${res1.body}`);
 
     expect(resApagada.statusCode).toEqual(StatusCodes.NO_CONTENT);
   });
 
   it("Apagar Registro Que não Existe", async () => {
-    const res1 = await testServer.delete("/cidades/9999").send();
+    const res1 = await testServer.delete("/pessoas/9999").send();
 
     expect(res1.statusCode).toEqual(StatusCodes.NO_CONTENT);
     expect(res1.body).toHaveProperty("errors");
--- tests/pessoas/GetAll.test.ts
+++ tests/pessoas/GetAll.test.ts
@@ -2,15 +2,30 @@ import { StatusCodes } from "http-status-codes";
 import { testServer } from "../jest.setup";
 
 describe("Pessoas - GetAll - Retornar todas as Pessoas.", () => {
-  it("Apagar Registro", async () => {
-    const res1 = await testServer.get("/cidades").send({});
+  let cidadeId: number | undefined = undefined;
 
-    expect(res1.statusCode).toEqual(StatusCodes.OK);
+  beforeAll(async () => {
+    const resCidade = await testServer.post("/cidades").send({
+      nome: "Teste",
+    });
 
-    const ResGetAll = await testServer.get("/cidades").send();
+    cidadeId = resCidade.body;
+  });
 
-    expect(Number(ResGetAll.header["x-total-count"])).toBeGreaterThan(0);
+  it("Buscar Registro", async () => {
+    const res1 = await testServer.post("/pessoas").send({
+      name: "Thiago",
+      sobrenome: "Mata",
+      email: "taiagomata@gmail.com",
+      cidadeId,
+    });
+
+    expect(res1.statusCode).toEqual(StatusCodes.CREATED);
+
+    const ResGetAll = await testServer.get("/pessoas").send({});
+
     expect(ResGetAll.statusCode).toEqual(StatusCodes.OK);
+    expect(Number(ResGetAll.header["x-total-count"])).toBeGreaterThan(0);
     expect(ResGetAll.body.length).toBeGreaterThan(0);
   });
 });
